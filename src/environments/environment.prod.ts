02c6e3283d65d58df84cce4e6ed9c6519d19a722
dgnyldrm00 <78863150+dgnyldrm00@users.noreply.github.com>
2021-02-10 23:07:26 +0300

    facebookkopy

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDLw5bzk89mcCspnyRDDIKBaVLoanbpilE",
+    authDomain: "facebook-6e7e8.firebaseapp.com",
+    databaseURL: "https://facebook-6e7e8-default-rtdb.firebaseio.com",
+    projectId: "facebook-6e7e8",
+    storageBucket: "facebook-6e7e8.appspot.com",
+    messagingSenderId: "818589592596",
+    appId: "1:818589592596:web:cf37ef9a20d6386f5c4b01"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDLw5bzk89mcCspnyRDDIKBaVLoanbpilE",
+    authDomain: "facebook-6e7e8.firebaseapp.com",
+    databaseURL: "https://facebook-6e7e8-default-rtdb.firebaseio.com",
+    projectId: "facebook-6e7e8",
+    storageBucket: "facebook-6e7e8.appspot.com",
+    messagingSenderId: "818589592596",
+    appId: "1:818589592596:web:cf37ef9a20d6386f5c4b01"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
